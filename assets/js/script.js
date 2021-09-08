35cea1b739cef418e582024e5173a7e1506e5ccc
SergeyVishnevskiy <serrzik13manager@gmail.com>
2021-09-08 22:15:21 +0300

    in process

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,21 @@
 let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
-// document.documentElement.style.setProperty('--vh', `${vh}px`);
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-$(function () {
-  $('*[placeholder="Номер телефону"]*').mask('+3 (000) 000 00 00');
+$(document).ready(function () {
+  $('.contacts').hover(function () {
+    $('.phone-list').toggleClass('active');
+  });
+  $('.categories-list .categories-item').hover(function () {
+    $(this).find('.categoria-list').toggleClass('active');
+  });
+
+  $('.list-item__link').hover(function () {
+    $('.list-item__link').removeClass('active');
+    $(this).toggleClass('active');
+  });
+
+  $(function () {
+    $('*[placeholder="Номер телефону"]*').mask('+3 (000) 000 00 00');
+  });
 });
